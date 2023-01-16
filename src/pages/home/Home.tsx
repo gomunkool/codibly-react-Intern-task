b253a0ad3f546cf7a8715d9d8b636b94f8af83e1
gomunkool <pivzz19881988@gmil.com>
2023-01-16 23:50:31 +0300

    feat: add product component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { HomeBox, FilterTextField } from './Home.styled';
-import { Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import axios from 'axios';
 import ProductItem from '../../components/product/ProductItem';
 
+export type Product = {
+  id: number;
+  name: string;
+  color: string;
+  year: number;
+  pantone_value: string;
+};
+
+interface IProducts {
+  ProductList: Product[];
+}
+
 const Home = (): JSX.Element => {
   const [products, setProducts] = useState([]);
-
   useEffect(() => {
     try {
       axios.get('https://reqres.in/api/products').then((resp) => {
@@ -18,11 +29,37 @@ const Home = (): JSX.Element => {
     }
   }, []);
 
+  const renderProducts = () => {
+    return products.map(({ id, name, color, year, pantone_value }: Product): JSX.Element => {
+      console.log(id);
+      return (
+        <Grid key={id} item xs={6}>
+          <Box>1</Box>
+        </Grid>
+      );
+    });
+  };
+
   return (
     <HomeBox>
       <Typography variant={'h4'}>The Codibly's task</Typography>
       <FilterTextField id="outlined-basic" label="Filter-only numbers" variant="outlined" />
-      <ProductItem></ProductItem>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        {renderProducts()}
+        {/* 
+        <Grid item xs={6}>
+          <Box>1</Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Box>2</Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Box>3</Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Box>4</Box>
+        // </Grid> */}
+      </Grid>
     </HomeBox>
   );
 };
