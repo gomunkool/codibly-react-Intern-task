98cff6f17e01a7c4415afa818f75c2a713b45a15
gomunkool <pivzz19881988@gmil.com>
2023-01-16 16:59:57 +0300

    add: prettier and eslintrc

---
+++ src/pages/home/Home.styled.ts
@@ -0,0 +1,8 @@
+import { styled } from '@mui/material/styles';
+import { Box} from '@mui/material';
+
+
+export const HomeBox = styled(Box)(({  }) => ({
+margin:'0 auto'
+}));
+    
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import { HomeBox } from './Home.styled';
+
+const Home = (): JSX.Element => {
+  return (
+    <HomeBox>
+      <Typography>sdfsdf</Typography>
+    </HomeBox>
+  );
+};
+
+export default Home;
